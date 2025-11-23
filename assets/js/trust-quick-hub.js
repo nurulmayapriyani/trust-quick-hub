fc0dc10497151b1e9f0115c9c73906f81abd44c5
nurulmayapriyani <nurulmayapriyani@gmail.com>
2025-11-24 00:25:44 +0700

    remove notif built astro

--- assets/js/trust-quick-hub.js
+++ assets/js/trust-quick-hub.js
@@ -26,16 +26,6 @@
       var sidebar = document.querySelector('.sidenav');
       var ps1 = new PerfectScrollbar(sidebar);
     };
-
-    if (document.getElementsByClassName('navbar-collapse')[0]) {
-      var fixedplugin = document.querySelector('.navbar:not(.navbar-expand-lg) .navbar-collapse');
-      var ps2 = new PerfectScrollbar(fixedplugin);
-    };
-
-    if (document.getElementsByClassName('fixed-plugin')[0]) {
-      var fixedplugin = document.querySelector('.fixed-plugin');
-      var ps3 = new PerfectScrollbar(fixedplugin);
-    };
   };
 })();
 
@@ -53,57 +43,6 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
-
-// Fixed Plugin
-
-if (document.querySelector('.fixed-plugin')) {
-  var fixedPlugin = document.querySelector('.fixed-plugin');
-  var fixedPluginButton = document.querySelector('.fixed-plugin-button');
-  var fixedPluginButtonNav = document.querySelector('.fixed-plugin-button-nav');
-  var fixedPluginCard = document.querySelector('.fixed-plugin .card');
-  var fixedPluginCloseButton = document.querySelectorAll('.fixed-plugin-close-button');
-  var navbar = document.getElementById('navbarBlur');
-  var buttonNavbarFixed = document.getElementById('navbarFixed');
-
-  if (fixedPluginButton) {
-    fixedPluginButton.onclick = function() {
-      if (!fixedPlugin.classList.contains('show')) {
-        fixedPlugin.classList.add('show');
-      } else {
-        fixedPlugin.classList.remove('show');
-      }
-    }
-  }
-
-  if (fixedPluginButtonNav) {
-    fixedPluginButtonNav.onclick = function() {
-      if (!fixedPlugin.classList.contains('show')) {
-        fixedPlugin.classList.add('show');
-      } else {
-        fixedPlugin.classList.remove('show');
-      }
-    }
-  }
-
-  fixedPluginCloseButton.forEach(function(el) {
-    el.onclick = function() {
-      fixedPlugin.classList.remove('show');
-    }
-  })
-
-  document.querySelector('body').onclick = function(e) {
-    if (e.target != fixedPluginButton && e.target != fixedPluginButtonNav && e.target.closest('.fixed-plugin .card') != fixedPluginCard) {
-      fixedPlugin.classList.remove('show');
-    }
-  }
-
-  if (navbar) {
-    if (navbar.getAttribute('navbar-scroll') == 'true') {
-      buttonNavbarFixed.setAttribute("checked", "true");
-    }
-  }
-
-}
 
 // Tabs navigation
 
